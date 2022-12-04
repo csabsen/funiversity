5c2950d2ffd7d2a3f3d9708a8df9452eca1fed2a
Csaba Nagy <csabsen@gmail.com>
2022-12-04 09:40:39 +0100

    34. Manipulating Default URL Based Queries

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -20,7 +20,7 @@
     <?php
     $today = date('Ymd');
     $events = new WP_Query([
-    	'posts_per_page' => -1,
+    	'posts_per_page' => 2,
     	'post_type' => 'event',
     	'orderby' => 'meta_value_num',
     	'meta_key' => 'event_date',
@@ -96,7 +96,7 @@
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h5>
               <p>
                 <?php if (has_excerpt()) {
-                	echo the_excerpt();
+                	echo get_the_excerpt();
                 } else {
                 	echo wp_trim_words(get_the_content(), 18);
                 } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
